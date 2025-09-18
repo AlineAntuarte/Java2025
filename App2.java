0fcd63b8b73b89fe0b6a750f743c2af70363e337
AlineAntuarte <aline.ba@aluno.ifsc.edu.br>
2025-09-18 20:21:02 -0300

    Exercício 2

--- App2.java
+++ App2.java
@@ -17,35 +17,74 @@ public class App2 {
          * 
          * System.out.println(resultado);
          * System.out.printf("%.4f \n",resultado);
-         
-
-        int a = 1;
-        int b = 2;
-        
+         * 
+         * 
+         * int a = 1;
+         * int b = 2;
+         * 
          * a = a + 1;
          * a += 1;
          * a++;
          * a--;
          */
-        System.out.println("Insira um número: ");
-         int num = teclado.nextInt();
 
-        if (num == 1) {
-            System.out.println("Um");
-        }else if (num == 2) {
-            System.out.println("Dois");
-        }else if (num == 3) {
-            System.out.println("Três");
-        }else if (num == 4) {
-            System.out.println("Quatro");
-        }else{
-            System.out.println("O número inserido não é 1, 2, 3 e nem 4.");
+        // Encadeado
+        System.out.println("Insira um número: ");
+        int num = teclado.nextInt();
+
+        /*
+         * if (num == 1) {
+         * System.out.println("Um");
+         * } else if (num == 2) {
+         * System.out.println("Dois");
+         * } else if (num == 3) {
+         * System.out.println("Três");
+         * } else if (num == 4) {
+         * System.out.println("Quatro");
+         * } else {
+         * System.out.println("O número inserido não é 1, 2, 3 e nem 4.");
+         * }
+         */
+
+        switch (num) {
+            case 1:
+                System.out.println("Um");
+                break;
+
+            case 2:
+                System.out.println("Dois");
+                break;
+
+            case 3:
+                System.out.println("Três");
+                break;
+
+            case 4:
+                System.out.println("Quatro");
+                break;
+
+            default:
+                System.out.println("O número inserido não é 1, 2, 3 e nem 4.");
+                break;
         }
 
+        switch (num) {
+            case 1 -> System.out.println("Um");
+            case 2 -> System.out.println("Dois");
+            case 3 -> System.out.println("Três");
+            case 4 -> System.out.println("Quatro");
+            default -> System.out.println("Número desconhecido");
+        }
 
-
-
-
+        System.out.println("Insira um número: ");
+            
+        String res = switch (teclado.nextInt()) {
+            case 1 -> "Um";
+            case 2 -> "Dois";
+            case 3 -> "Três";
+            case 4 -> "Quatro";
+            default -> "Nenhum de 1 a 4";
+        };
 
         teclado.close();
     }
